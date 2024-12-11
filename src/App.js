5a9c120ea5f246de0749f85ce16cbd9adc28348e
Fionn Delahunty <Fionndelahunty@gmail.com>
2024-12-11 14:14:15 +0000

    fix: vercel tracking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Production from "./pages/Production";
@@ -9,17 +9,18 @@ import Compare from "./pages/Compare";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="Production" element={<Production />} />
-                <Route path="Insights" element={<Insights />} />
-                <Route path="Contribute" element={<Contribute />} />
-                <Route path="Compare" element={<Compare />} />
-
-            </Routes>
-        </BrowserRouter>
-    <Analytics />
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="Production" element={<Production />} />
+                    <Route path="Insights" element={<Insights />} />
+                    <Route path="Contribute" element={<Contribute />} />
+                    <Route path="Compare" element={<Compare />} />
+                </Routes>
+            </BrowserRouter>
+            <Analytics />
+        </>
     );
 }
 
